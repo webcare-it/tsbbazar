262bd45910b3813ade43789866cdc30546c0eec1
Md Arman Ali <ceowebcareit@gmail.com>
2025-12-04 17:59:37 +0600

    sdgsdfg

--- resources/views/components/alert.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="{{ $backgroundCSS() }} alert-dismissible fade show" role="alert">
-    <strong></strong> {{ $message }}.
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
--- resources/views/components/label.blade.php
+++
@@ -1,3 +0,0 @@
-<label {{ $attributes }}> 
-    {{ $slot }}
-</label>
